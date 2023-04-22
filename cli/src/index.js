c5c94a8e3945fe9c78cd13b4c432734797090e00
Camel Aissani <camel.aissani@gmail.com>
2023-04-22 22:52:39 +0200

    worked on task #106

--- cli/src/index.js
+++ cli/src/index.js
@@ -20,16 +20,30 @@ const {
 } = require('./commands');
 const { parseEnv } = require('./utils');
 
-const argv = minimist(process.argv.slice(2));
-
-const Main = async () => {
-  process.on('SIGINT', () => {
-    // do nothing on SIGINT to let the child process to handle the signal
-  });
-
+function getArgs() {
+  const argv = minimist(process.argv.slice(2));
   const command = argv._.length ? argv._[0] : '';
+  const helpArg = argv.h || argv.help;
+  const ciArg = !!argv.ci;
+  const serviceArg = argv.service || argv.s;
 
-  displayHeader();
+  // checks that ciArg is passed only with the build and start commands
+  if (!['build', 'start'].includes(command) && ciArg) {
+    console.error(
+      chalk.red(
+        'The --ci option is only available with the build and start commands'
+      )
+    );
+    process.exit(1);
+  }
+
+  // checks that serviceArg is passed only with the build command
+  if (command !== 'build' && serviceArg) {
+    console.error(
+      chalk.red('The --service option is only available with the build command')
+    );
+    process.exit(1);
+  }
 
   if (
     ![
@@ -43,77 +57,87 @@ const Main = async () => {
       'dumpdb',
     ].includes(command)
   ) {
-    return displayHelp();
+    displayHelp();
+    return process.exit(1);
   }
 
-  let envConfig = null;
-  if (fs.existsSync(path.resolve(process.cwd(), '.env'))) {
-    console.log('Found .env file and rely on it to run\n');
-    envConfig = parseEnv();
+  return {
+    command,
+    helpArg,
+    ciArg,
+    serviceArg,
+  };
+}
 
-    // for backward compatibility
-    if (envConfig?.NGINX_PORT) {
-      envConfig.GATEWAY_PORT = envConfig.NGINX_PORT;
-      delete envConfig.NGINX_PORT;
-    }
+async function main() {
+  process.on('SIGINT', () => {
+    // do nothing on SIGINT to let the child process (docker-compose) to handle the signal
+  });
+
+  const { command, helpArg, ciArg, serviceArg } = getArgs();
+
+  if (helpArg) {
+    displayHelp();
+    return process.exit(0);
   }
 
-  const promptsConfig = await askForEnvironmentVariables(envConfig);
-  writeDotEnv(promptsConfig, envConfig);
+  displayHeader({ ci: ciArg });
 
-  switch (command) {
-    case 'build':
-      await stop();
-      await build();
-      break;
-    case 'start':
-      await start();
-      break;
-    case 'stop':
-      await stop();
-      break;
-    case 'dev':
-      await stop({ runMode: 'dev' });
-      await dev();
-      break;
-    case 'status':
-      await status();
-      break;
-    case 'config': {
-      const { runMode = 'prod' } = await askRunMode();
-      await config(runMode);
-      break;
-    }
-    case 'restoredb': {
-      const backupFiles = [];
-      try {
-        const files = fs.readdirSync(
-          path.resolve(process.execPath, '..', 'backup')
-        );
-        files
-          .filter((file) => file.endsWith('.dump'))
-          .forEach((file) => {
-            backupFiles.push(file);
-          });
-      } catch (error) {
-        console.error(chalk.red(error));
+  if (!ciArg) {
+    let envConfig = null;
+    if (fs.existsSync(path.resolve(process.cwd(), '.env'))) {
+      console.log('Found .env file and rely on it to run\n');
+      envConfig = parseEnv();
+
+      // for backward compatibility
+      if (envConfig?.NGINX_PORT) {
+        envConfig.GATEWAY_PORT = envConfig.NGINX_PORT;
+        delete envConfig.NGINX_PORT;
       }
-
-      if (backupFiles.length === 0) {
-        console.error(chalk.red('No dump files found in the backup directory'));
-        return;
-      }
-
-      const { backupFile } = await askBackupFile(backupFiles);
-      await restoreDB(backupFile);
-      break;
     }
-    case 'dumpdb':
-      await dumpDB();
-      break;
-    default:
-      displayHelp();
+    const promptsConfig = await askForEnvironmentVariables(envConfig);
+    writeDotEnv(promptsConfig, envConfig);
   }
-};
 
-Main();
+  try {
+    switch (command) {
+      case 'build':
+        await stop({ ci: ciArg });
+        await build({ ci: ciArg, service: serviceArg });
+        break;
+      case 'start':
+        await start({ ci: ciArg });
+        break;
+      case 'stop':
+        await stop({ ci: ciArg });
+        break;
+      case 'dev':
+        await stop({ ci: false, runMode: 'dev' });
+        await dev();
+        break;
+      case 'status':
+        await status();
+        break;
+      case 'config': {
+        const { runMode = 'prod' } = await askRunMode();
+        await config(runMode);
+        break;
+      }
+      case 'restoredb': {
+        const { backupFile } = await askBackupFile();
+        await restoreDB(backupFile);
+        break;
+      }
+      case 'dumpdb':
+        await dumpDB();
+        break;
+      default: // do nothing
+    }
+  } catch (error) {
+    console.error(chalk.red(error.stack || error));
+    process.exit(1);
+  }
+  process.exit(0);
+}
+
+main();
